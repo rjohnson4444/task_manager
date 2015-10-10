c86136452f86c82765004e91f844d764e4d2cd48
Ryan Johnson <rjohnson4444@live.com>
2015-10-09 21:04:32 -0600

    Added views and form inside new view.

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -4,4 +4,17 @@ class TaskManagerApp < Sinatra::Base
   get '/' do
     erb :dashboard
   end
+
+  get '/tasks' do
+    @tasks = ['task1', 'task2', 'task3']
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
 end
