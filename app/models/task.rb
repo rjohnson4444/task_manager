a5527b0604e290cc6869f81393449e6334f4c671
Ryan Johnson <rjohnson4444@live.com>
2015-10-09 20:07:20 -0600

    Added gemfile and config.ru file second time around

--- app/models/task.rb
+++
@@ -1,9 +0,0 @@
-class Task
-  attr_reader :id, :title, :description
-  def initialize(data)
-    @id          = data['id']
-    @title       = data['title']
-    @description = data['description']
-  end
-
-end
--- config.ru
+++ config.ru
@@ -3,6 +3,6 @@ Bundler.require
 
 $LOAD_PATH.unshift(File.expand_path("app", __dir__))
 
-require 'controllers/task_manager_app'
+require 'contollers/task_manager_app'
 
 run TaskManagerApp
